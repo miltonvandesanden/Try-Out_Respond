33ae91552c136a7e44ed0ed730e598811dd74f5f
Valentinevdw <valentine_vanderwiel@msn.com>
2017-02-14 15:01:34 +0100

    Layout login + Refreshtokens

--- Tryout_Login/Tryout_Login/Controllers/LoginController.cs
+++ Tryout_Login/Tryout_Login/Controllers/LoginController.cs
@@ -6,16 +6,26 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Tryout_Login.Models;
+using static System.Collections.Specialized.BitVector32;
 
 namespace Tryout_Login.Controllers
 {
     public class LoginController : Controller
     {
         string prefix = "http://localhost:54295/api/accounts/";
+        string token;
 
         // GET: Login
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ViewResult InfoAllAccounts()
+        {
+            PostRefreshToken();
+            GetAllAccounts();
+
             return View();
         }
 
@@ -25,30 +35,24 @@ namespace Tryout_Login.Controllers
         }
 
         [HttpPost]
-        public void Registreer(LoginViewModel model)
+        public ActionResult Registreer(LoginViewModel model)
         {
-            Login(model);
+            PostRegister(model);
+
+            return View("~/Views/Login/Login_Result.cshtml");
         }
 
         public ViewResult Login()
         {
             return View();
         }
-
+        
         [HttpPost]
         public ActionResult Login(LoginViewModel model)
         {
-            if (model.password == null)
-            {
-                PostRegister(model);
-            }
-            else
-            {
-                PostAuth(model);
-            }
+            PostAuth(model);
 
             return View("~/Views/Login/Login_Result.cshtml");
-
         }
 
         public void PostRegister(LoginViewModel model)
@@ -96,7 +100,152 @@ namespace Tryout_Login.Controllers
                 }
             }
 
+            token = result;
+
+            Session["currentToken"] = token;
+
             ViewBag.Items = result;
         }
+
+        public void PostRefreshToken()
+        {
+            token = Session["currentToken"].ToString();
+            token = token.TrimEnd('"');
+            token = token.TrimStart('"');
+            string url = prefix + "refreshToken";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+
+            request.Headers.Add("token", token);
+            request.ContentLength = 0;
+
+            string result;
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+
+            token = result;
+            Session["currentToken"] = token;
+            token = Session["currentToken"].ToString();
+            token = token.TrimEnd('"');
+            token = token.TrimStart('"');
+        }
+
+        public void PostLogout()
+        {
+            string url = prefix + "logout";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+
+            request.Headers.Add("token", token);
+            request.ContentLength = 0;
+
+            string result;
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+
+            ViewBag.Logout = "Je bent uitgelogd" + result;
+        }
+
+        public void PostMakeAdmin()
+        {
+            string url = prefix + "{userID}/makeadmin";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+
+            request.Headers.Add("token", token);
+            request.ContentLength = 0;
+
+            string result;
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+
+            ViewBag.Logout = "Deze gebruiker is nu admin " + result;
+        }
+
+        public void GetUserID()
+        {
+            string url = prefix + "{userID}";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            request.Headers.Add("token", token);
+            request.ContentLength = 0;
+
+            string result;
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+
+            ViewBag.Logout = "Account info: " + result;
+        }
+
+        public void PostChangePassword(LoginViewModel model)
+        {
+            string url = prefix + "changePassword";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+
+            request.Headers.Add("token", token);
+            request.Headers.Add("password", model.password);
+            request.ContentLength = 0;
+
+            string result;
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+
+            ViewBag.Logout = "Je wachtwoord is gewijzigd: " + result;
+        }
+
+        public void GetAllAccounts()
+        {
+            string url = prefix + "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+
+            request.Headers.Add("token", token);
+            request.ContentLength = 0;
+
+            string result;
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+
+            ViewBag.Logout = "Account info: " + result;
+        }
+
     }
 }
